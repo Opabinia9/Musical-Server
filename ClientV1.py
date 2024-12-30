b48c9a2b9157e462af846a33bd5acb2d1dd361d9
Sebastion Price <sebastionprice@gmail.com>
2024-12-30 23:12:55 +1100

    added sound and code for server first contact

--- ClientV1.py
+++ ClientV1.py
@@ -1,10 +1,11 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 client.connect(("localhost", 9999))
 
 while True:
+    msg = client.recv(1024).decode('utf-8')
+    print(msg)
     client.send(input("Message: ").encode('utf-8'))
     msg = client.recv(1024).decode('utf-8')
     print(msg)
--- ServerV1.py
+++ ServerV1.py
@@ -1,4 +1,5 @@
 import socket
+from playsound import playsound
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("localhost", 9999))
@@ -8,6 +9,10 @@ server.listen()
 client, addr = server.accept()
 
 while True:
+    client.send("Please Select Track".encode('utf-8'))
     msg = client.recv(1024).decode('utf-8')
-    print(msg)
-    client.send(input("Message: ").encode('utf-8'))
+    print("Now Playing \"" + msg + "\"")
+    client.send(("Now Playing \"" + msg + "\"").encode('utf-8'))
+    playsound('C:\\Users\\sebas\\OneDrive\\Music\\new\\Billy Joel - A Matter of Trust.mp3')
+    print("Song \""+msg+"\" Finished Playing")
+    client.send(("Song \""+msg+"\" Finished Playing").encode('utf-8'))
