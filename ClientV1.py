e67a8933d79ba65bea7c1d159c4fd0b76e007e3c
Sebastion Price <sebastionprice@gmail.com>
2024-12-30 22:31:50 +1100

    first commit

---
+++ ClientV1.py
@@ -0,0 +1,10 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(("localhost", 9999))
+
+while True:
+    client.send(input("Message: ").encode('utf-8'))
+    msg = client.recv(1024).decode('utf-8')
+    print(msg)
---
+++ ServerV1.py
@@ -0,0 +1,13 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost", 9999))
+
+server.listen()
+
+client, addr = server.accept()
+
+while True:
+    msg = client.recv(1024).decode('utf-8')
+    print(msg)
+    client.send(input("Message: ").encode('utf-8'))
