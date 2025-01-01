4206039fa4c8d074172be2f5a7979ede2564f70e
Sebastion Price <sebastionprice@gmail.com>
2025-01-01 20:54:16 +1100

    added track list and selection

--- ServerV1.py
+++ ServerV1.py
@@ -1,18 +1,19 @@
 import socket
 from playsound import playsound
-
+songs = open("SongsList.txt", "r").read().split("\n")
+for i in range(len(songs)):
+    songs[i].replace("\"","'")
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("localhost", 9999))
-
 server.listen()
-
 client, addr = server.accept()
 
 while True:
     client.send("Please Select Track".encode('utf-8'))
     msg = client.recv(1024).decode('utf-8')
-    print("Now Playing \"" + msg + "\"")
-    client.send(("Now Playing \"" + msg + "\"").encode('utf-8'))
-    playsound('C:\\Users\\sebas\\OneDrive\\Music\\new\\Billy Joel - A Matter of Trust.mp3')
-    print("Song \""+msg+"\" Finished Playing")
-    client.send(("Song \""+msg+"\" Finished Playing").encode('utf-8'))
+    msg = int(msg)
+    print("Now Playing \""+songs[msg]+"\"")
+    client.send(("Now Playing \""+songs[msg]+"\"").encode('utf-8'))
+    playsound(songs[msg])
+    print("Song \""+songs[msg]+"\" Finished Playing")
+    client.send(("Song \""+songs[msg]+"\" Finished Playing").encode('utf-8'))
